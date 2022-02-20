5147d5bb9ea6b5878b9eea4593836cab86bc474a
snappdevelopment <se.neu@web.de>
2022-02-20 17:13:41 +0100

    Apply MacOS styling to Ui

--- src/main/kotlin/Ui.kt
+++ src/main/kotlin/Ui.kt
@@ -1,17 +1,25 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import arch.StateMachine
 import kotlinx.coroutines.flow.collect
+
+private val activeColor = Color(0xFF007AFF)
+private val inactiveColor = Color(0xFF8E8E93)
 
 @Composable
 @Preview
@@ -58,21 +66,21 @@ private fun Content(
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp)
         ) {
-            OutlinedTextField(
+            MacTextField(
                 modifier = Modifier.weight(1F),
+                placeholder = "Input pattern (e.g. <\$KEY><\$VALUE>)",
                 value = inputPatternValue,
-                onValueChange = { inputPatternValue = it },
-                placeholder = { Text(text = "Input pattern (e.g. <\$KEY><\$VALUE>)") },
+                onValueChanged = { inputPatternValue = it },
                 singleLine = true
             )
 
             Spacer(modifier = Modifier.width(16.dp))
 
-            OutlinedTextField(
+            MacTextField(
                 modifier = Modifier.weight(1F),
+                placeholder = "Output pattern (e.g. \$KEY: \$VALUE)",
                 value = outputPatternValue,
-                onValueChange = { outputPatternValue = it },
-                placeholder = { Text(text = "Output pattern (e.g. \$KEY: \$VALUE)") },
+                onValueChanged = { outputPatternValue = it },
                 singleLine = true
             )
         }
@@ -85,45 +93,55 @@ private fun Content(
                 .padding(horizontal = 16.dp)
         ) {
 
-            OutlinedTextField(
+            MacTextField(
                 modifier = Modifier
                     .fillMaxSize()
                     .weight(1F),
+                placeholder = "Input",
                 value = inputTextValue,
-                onValueChange = { inputTextValue = it },
-                placeholder = { Text(text = "Input") }
+                onValueChanged = { inputTextValue = it },
+                singleLine = false
             )
 
             Spacer(modifier = Modifier.width(16.dp))
 
-            OutlinedTextField(
+            MacTextField(
                 modifier = Modifier
                     .fillMaxSize()
                     .weight(1F),
-                value = state.output,
-                onValueChange = {},
-                readOnly = true,
-                placeholder = { Text(text = "Output") }
+                placeholder = "Output",
+                value = TextFieldValue(text = state.output),
+                onValueChanged = {},
+                singleLine = false
             )
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(
+        Surface(
             modifier = Modifier
                 .align(Alignment.End)
-                .padding(bottom = 16.dp, end = 16.dp),
-            onClick = {
-                sendAction(
-                    ConvertClicked(
-                        text = inputTextValue.text,
-                        inputPattern = inputPatternValue.text,
-                        outputPattern = outputPatternValue.text
+                .padding(bottom = 16.dp, end = 16.dp)
+                .clickable {
+                    sendAction(
+                        ConvertClicked(
+                            text = inputTextValue.text,
+                            inputPattern = inputPatternValue.text,
+                            outputPattern = outputPatternValue.text
+                        )
                     )
-                )
-            },
+                }
         ) {
-            Text(text = "Convert")
+            Text(
+                modifier = Modifier
+                    .defaultMinSize(minWidth = 157.dp, minHeight = 28.dp)
+                    .background(color = activeColor, shape = RoundedCornerShape(4.dp))
+                    .padding(vertical = 6.dp, horizontal = 10.dp),
+                text = "Convert",
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                color = Color.White,
+            )
         }
     }
 }
@@ -157,5 +175,33 @@ private fun Error(
             )
         },
         onDismissRequest = { sendAction(ErrorDismissed) }
+    )
+}
+
+@Composable
+private fun MacTextField(
+    modifier: Modifier,
+    placeholder: String,
+    value: TextFieldValue,
+    onValueChanged: (TextFieldValue) -> Unit,
+    singleLine: Boolean
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChanged,
+        placeholder = {
+            Text(
+                text = placeholder,
+                fontSize = 12.sp
+            )
+        },
+        singleLine = singleLine,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = activeColor,
+            unfocusedBorderColor = inactiveColor,
+            unfocusedLabelColor = inactiveColor,
+            focusedLabelColor = inactiveColor
+        )
     )
 }
